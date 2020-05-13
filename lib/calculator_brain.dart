7d59f4c1ef6ca8272c51fcbb4f9140820435b782
Edwin Villa <villaceniceros@gmail.com>
2020-05-13 13:47:55 -0600

    ADD three more bmi results

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -14,7 +14,13 @@ class CalculatorBrain {
   }
 
   String getResult() {
-    if (_bmi >= 25) {
+    if (_bmi >= 40) {
+      return 'Obesidad extrema';
+    } else if (_bmi > 35) {
+      return 'Obesidad severa';
+    } else if (_bmi > 30) {
+      return 'Obesidad';
+    } else if (_bmi > 25) {
       return 'Sobrepeso';
     } else if (_bmi > 18.5) {
       return 'Normal';
@@ -24,7 +30,13 @@ class CalculatorBrain {
   }
 
   String getInterpretation() {
-    if (_bmi >= 25) {
+    if (_bmi >= 40) {
+      return 'Tienes un nivel de obesidad alarmante, empieza ya a tomar acciones master 🤪';
+    } else if (_bmi > 35) {
+      return 'Tienes un nivel de obesidad preocupante, es momento de mejorar dieta y ejercicio crack 🥴';
+    } else if (_bmi > 30) {
+      return 'Tienes un indice por encima del sobrepeso, deberias cambiar tus habitos de dieta y ejercicio titan! 🤠';
+    } else if (_bmi > 25) {
       return 'Tienes un indice por encima de lo normal, intenta ejercitarte mas capo! 😆';
     } else if (_bmi > 18.5) {
       return 'Tienes un indice normal, bien hecho tigre! 😎';
